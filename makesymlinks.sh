96ee1dfb60caca2957fc692ea7d9aa2581705ab2
Felix Bello <felix.bello@becit.ch>
2015-10-19 22:03:26 +0200

    i3status config file

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,3 +6,5 @@ ln -s /home/felix/02-Git/git/dotfiles/vim/.vimrc /home/felix/.vimrc
 echo "vim dotfile symlink created"
 ln -s /home/felix/02-Git/git/dotfiles/i3/config /home/felix/.i3/config
 echo "i3 config file symlink created"
+ln -s /home/felix/02-Git/git/dotfiles/i3/i3status.conf /etc/i3status.conf
+echo "i3status config file symlink created"
