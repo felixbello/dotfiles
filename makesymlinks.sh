bc15af879d1c6f698ed85b76f7fad51c862305ab
Felix Bello <felix.bello@becit.ch>
2015-10-19 21:50:57 +0200

    symlinks creator

---
+++ makesymlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#################################
+# Symlinks creator by Felix Bello
+#################################
+ln -s /home/felix/02-Git/git/dotfiles/vim/.vimrc /home/felix/.vimrc
