ee29d4270976c16b029761a0a1de82a5599b6dad
Felix Bello <felix.bello@becit.ch>
2015-10-20 21:29:13 +0200

    wqsome checks for linux

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,9 +2,18 @@
 #################################
 # Symlinks creator by Felix Bello
 #################################
-ln -s /home/felix/02-Git/git/dotfiles/vim/.vimrc /home/felix/.vimrc
-echo "vim dotfile symlink created"
-ln -s /home/felix/02-Git/git/dotfiles/i3/config /home/felix/.i3/config
-echo "i3 config file symlink created"
-ln -s /home/felix/02-Git/git/dotfiles/i3/i3status.conf /etc/i3status.conf
-echo "i3status config file symlink created"
+#ln -s /home/felix/02-Git/git/dotfiles/i3/i3status.conf /etc/i3status.conf
+#echo "i3status config file symlink created"
+
+platform=$(uname);
+
+if [[ $platform == 'Linux']]; then
+		echo "its linux!"
+		ln -s /home/felix/02-Git/git/dotfiles/vim/.vimrc /home/felix/.vimrc
+		echo "vim done!"
+		ln -s /home/felix/02-Git/git/dotfiles/i3/config /home/felix/.i3/config
+		echo "i3 done!"
+		# TODO! copy i3status to /etc/i3status
+	elf [[ $platform == 'Darwin']]; then
+		echo "its OSX!"
+fi
