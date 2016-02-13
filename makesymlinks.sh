3d13582225b512572b6102f06f34bfd4e741ac7b
felix <felix.bello@becit.ch>
2016-02-13 10:23:57 +0000

    adding user root check

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,23 +7,32 @@ platform=$(uname);
 
 # check operating system
 if [ $platform == 'Linux' ];then
+	if [ $USER == 'root' ];then
+		echo "U're root!! That's Danger!!"
 		echo "its linux!"
-		ln -s /home/felix/02-Git/product/dotfiles/vim/.vimrc /home/felix/.vimrc
+		ln -s /$USER/02-Git/product/dotfiles/vim/.vimrc /$USER/.vimrc
 		echo "vim done!"
-		ln -s /home/felix/02-Git/product/dotfiles/i3/config /home/felix/.i3/config
+		ln -s /$USER/02-Git/product/dotfiles/i3/config /$USER/.i3/config
 		echo "i3 done!"
 		# TODO! copy i3status to /etc/i3status
-
-elif [ $platform == 'Darwin' ]; then
-	echo "its OSX, lets do it!"
-	ln -s /Users/felix/02-Git/product/dotfiles/vim/.vimrc /Users/felix/.vimrc
-    ln -s /Users/felix/02-Git/product/dotfiles/productvim/.vim/bundle  /Users/felix/.vim
-    cd /Users/felix/02-Git/product/dotfiles/vim/.vim/bundle
-    git clone https://github.com/kien/ctrlp.vim.git
-    git clone https://github.com/klen/python-mode
-    cd /Users/felix/02-Git/product/dotfiles/vim/.vim/ftplugin
-	echo "vim done!"
+	else
+		echo "its linux!"
+		ln -s /home/$USER/02-Git/product/dotfiles/vim/.vimrc	/home/$USER/.vimrc
+		echo "vim done!"
+		ln -s /home/$USER/02-Git/product/dotfiles/i3/config	/home/$USER/.i3/config
+		echo "i3 done!"
+	fi
+		elif [ $platform == 'Darwin' ]; then
+			echo "its OSX, lets do it!"
+			ln -s /Users/felix/02-Git/product/dotfiles/vim/.vimrc /Users/felix/.vimrc
+    			ln -s /Users/felix/02-Git/product/dotfiles/productvim/.vim/bundle  /Users/felix/.vim
+    			cd /Users/felix/02-Git/product/dotfiles/vim/.vim/bundle
+    			git clone https://github.com/kien/ctrlp.vim.git
+    			git clone https://github.com/klen/python-mode
+    			cd /Users/felix/02-Git/product/dotfiles/vim/.vim/ftplugin
+			echo "vim done!"
 #else
 #	echo "maybe m$?!"
+
 fi
 echo "finished!"
